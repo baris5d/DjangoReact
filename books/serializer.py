0cf74f41da13052f86d9cdfe54efd5964ce2145a
baris5d <baris@barisdede.com>
2020-07-19 17:51:50 +0300

    Djangorestframework practice

--- books/serializer.py
+++ books/serializer.py
@@ -1,3 +1,5 @@
+from django.conf import settings 
+
 from rest_framework import serializers
 
 from .models import Books
--- books/views.py
+++ books/views.py
@@ -1,21 +1,32 @@
+from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render,redirect
 from django.utils.http import is_safe_url
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from .models import Books
 from .form import BooksForm
+from django.conf import settings 
+from .serializer import BooksSerializer
 
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
 
+@api_view(['GET'])
 def books_list(request, *args, **kwargs):
     blist = Books.objects.all()
     books = [x.serialize() for x in blist]
     data = {
         "response" : books
     }
-    return JsonResponse(data)
+    return Response(data)
 
+@api_view(['GET'])
 def book_detail_view(request, book_id, *args, **kwargs):
     data = { 
         "id" : book_id
@@ -30,16 +41,24 @@ def book_detail_view(request, book_id, *args, **kwargs):
     except:
         data['message'] = "Not Found"
         status = 404
-    return JsonResponse(data,status=status) 
+    return Response(data,status=status) 
 
-
+@api_view(['POST'])
 def book_create_view(request, *args, **kwargs):
+    serializer = BooksSerializer(data=request.POST or None)
+    if serializer.is_valid(raise_exception = True):
+        serializer.save()
+        return Response(serializer.data)
+    return Response({}, status=400)
+
+# without serializer
+def book_create_view_pure(request, *args, **kwargs):
     form = BooksForm(request.POST or None)
     next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
-        if next_url != None and is_safe_url(next_url):
+        if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form = BooksForm()
     return render(request, 'components/form.html', context={'form': form})
