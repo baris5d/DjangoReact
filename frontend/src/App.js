d3e47f69b8f76ad593efb853b55171708841cc64
baris5d <baris@barisdede.com>
2020-07-21 13:30:32 +0300

    Hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
   const[books,setBooks] = useState([])
 
+  useEffect (() => {
+    setBooks([{"name":"Otostopçunun Galaksi Rehberi", "author": "Douglas Adams", "description": "Lorem ipsum"},{
+      "name":"Hikayeler", 
+      "author":    "Edgar Allan Poe", 
+      "description": "Lorem ipsum sit door amet"
+    }])
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {books.map((book,index) => {
-            return <li>{book.name}</li>
-          })}
-        </p>
+        <h1>DjangoReact</h1>
       </header>
+      <div className="book-list">
+          {books.map((book,index) => {
+            return (
+            <div className="book-item">
+                <h2>{book.name}</h2>
+                <p>{book.author}</p>
+                <p>{book.description}</p>
+              </div>
+            )}
+          )}
+      </div>
     </div>
   );
 }
